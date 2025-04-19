639b8c86ce20c47d0310b30da7fc196a061a803e
yongbot <ymhan0724@gmail.com>
2025-04-20 01:19:32 +0900

    색 보정

--- src/stream/stream_server.py
+++ src/stream/stream_server.py
@@ -12,10 +12,12 @@ picam2.start()
 def generate_frames():
     while True:
         frame = picam2.capture_array()
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         _, buffer = cv2.imencode('.jpg', frame)
         frame_bytes = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
 
 @app.route('/')
 def index():
