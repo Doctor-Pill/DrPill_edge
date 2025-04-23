af3d6c3ac2e3e3a9bdd5658556e4e81a86d461cd
yongbot <ymhan0724@gamil.com>
2025-04-23 20:49:19 +0900

    recoder

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.signal.sender import send_signal
+from src.utils.recorder import record_audio
 
 if __name__ == '__main__':
-    print("📡 서버에 신호 전송 시도 중...")
-    send_signal()
+    # print("📡 서버에 신호 전송 시도 중...")
+    # send_signal()
 
+    record_audio(filename='my_voice.wav', duration=4)
