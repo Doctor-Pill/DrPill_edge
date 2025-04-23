f2c64c1be746ccf69c1239c72dc818309394cef0
yongbot <ymhan0724@gamil.com>
2025-04-23 23:12:55 +0900

    stt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from src.stt.stt_engine import listen
+from src.utils.recorder import record_audio
+from src.signal.sender import send_audio
 
 if __name__ == '__main__':
-    result = listen()
-    if result:
-        print("✅ 최종 인식 결과:", result)
+    filename = 'my_voice.wav'
+    record_audio(filename=filename, duration=5)
+    send_audio(filename)
