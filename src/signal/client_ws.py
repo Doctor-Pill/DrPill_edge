38c8cfe23c7e8653ab6dbd776341141ac499e2fe
yongbot <ymhan0724@gamil.com>
2025-04-24 04:15:54 +0900

    network connect

--- src/signal/client_ws.py
+++ src/signal/client_ws.py
@@ -1,4 +1,5 @@
 import socketio
+import subprocess
 
 sio = socketio.Client()
 
@@ -10,13 +11,16 @@ def connect():
 def disconnect():
     print("🔌 서버와의 연결이 끊어졌습니다.")
 
-@sio.event
-def connect_error(data):
-    print("❌ 서버 연결 중 오류 발생:", data)
-
 def connect_to_server():
     try:
         sio.connect("http://192.168.0.10:5000")
         print("✅ WebSocket 연결 시도 완료")
+
+        # 브라우저도 띄우기 (백그라운드)
+        subprocess.Popen([
+            "chromium-browser", "--kiosk", "http://192.168.0.10:5000", "--no-sandbox"
+        ])
+        print("🌐 브라우저 띄우기 완료")
+
     except Exception as e:
         print(f"❌ 서버 접속 실패: {e}")
