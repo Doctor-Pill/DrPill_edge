9f38064ff35ee3ebba2fc00339fca284e36ed92a
yongbot <ymhan0724@gamil.com>
2025-04-29 00:54:14 +0900

    new

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # 📍 DrPill_edge/main.py
 
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))  # 🔥 추가!
+
 from src.connection.socket_client import connect_to_server
 from src.control.browser_controller import open_browser
 
 if __name__ == "__main__":
-    open_browser()  # 부팅되면 항상 브라우저 오픈
-    connect_to_server()  # 서버 연결 및 명령 대기
+    open_browser()
+    connect_to_server()
