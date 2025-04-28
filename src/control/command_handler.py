75e4ddd6117142e339b6cfb8794032d8a441af86
yongbot <ymhan0724@gamil.com>
2025-04-29 06:20:32 +0900

    send

--- src/control/command_handler.py
+++ src/control/command_handler.py
@@ -2,6 +2,8 @@
 
 from src.control.browser_controller import open_browser, close_browser
 from src.control.camera_controller import (
+    open_cameras,
+    close_cameras,
     start_usb_streaming,
     start_picam_streaming,
     stop_all_streaming
@@ -12,6 +14,10 @@ def handle_command(command):
         open_browser()
     elif command == "close_browser":
         close_browser()
+    elif command == "open_cameras":
+        open_cameras()
+    elif command == "close_cameras":
+        close_cameras()
     elif command == "start_usb_streaming":
         start_usb_streaming()
     elif command == "start_picam_streaming":
