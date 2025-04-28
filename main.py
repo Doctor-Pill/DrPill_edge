64cd71490b19441e83d989db9824906dca2f53f5
yongbot <ymhan0724@gamil.com>
2025-04-28 23:30:45 +0900

    new

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # 📍 DrPill_edge/main.py
+import atexit
 
 from src.connection.socket_client import connect_to_server
+from src.control.browser_controller import open_browser, close_browser
+
+open_browser()
+atexit.register(close_browser)
 
 if __name__ == "__main__":
     connect_to_server()
